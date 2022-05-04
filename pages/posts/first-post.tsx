2123f42d24f3da73414b450e778a9f84b6017fca
etpotato <mareev.pv@gmail.com>
2022-05-04 22:42:22 +0300

    Add image, head and third party script

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script';
 
 const FirstPost: NextPage = () => {
-  console.log(navigator.userAgent);
   return (
     <>
+      <Head>
+        <title>First post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log(`script loaded correctly, window.FB has been populated`)
+          console.log(FB)
+        }}
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
